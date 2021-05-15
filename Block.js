59e65337b6bcde0e3f85d2eef0b6d804aeb0a63b
kimichen08 <69221428+kimichen08@users.noreply.github.com>
2021-05-15 13:14:58 -0700

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,31 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+        restitution :0.4,
+        friction :0.0,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.image = loadImage("block.png");
+      World.add(world, this.body);
+      this.visibility = 355;
+    }
+    display(){
+      var pos =this.body.position;
+      if (this.body.speed<3){
+        push();
+        translate(pos.x, pos.y);
+        imageMode(CENTER);
+        image(this.image,0,0,this.width,this.height);
+        pop();
+      } else{
+          World.remove(world,this.body);
+          push();
+          this.visibility = this.visibility-5;
+          tint(255,this.visibility);
+          image(this.image,this.body.position.x,this.body.position.y,30,40);
+          pop(); 
+        }
+    }
+};
